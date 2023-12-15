b32394a9855c1abdfc9e7a341e87bc493783f517
RusKayskas <KayskasRus@yandex.ru>
2023-12-16 02:17:00 +0300

    add some modules for styles

---
+++ components/layout/Sidebar.vue
@@ -0,0 +1,15 @@
+<template>
+    <aside class="px-5 py-8 bg-sidebar h-full relative w-full">
+        <NuxtLink to="/">
+           <NuxtImg class="mx-auto" src="/logo.svg" alt="" width="100px"></NuxtImg>
+        </NuxtLink>
+    </aside>
+</template>
+
+<script lang="ts" setup>
+
+</script>
+
+<style scoped>
+
+</style>
---
+++ layouts/default.vue
@@ -0,0 +1,22 @@
+<template>
+  <section class="grid">
+      <LayoutSidebar />
+      <div>
+          <slot/>
+      </div>
+  </section>
+</template>
+
+<script lang="ts" setup>
+
+
+  
+</script>
+
+<style scoped>
+  .grid {
+      min-height: 100vh;
+      display: grid;
+      grid-template-columns: 1fr 6fr;
+  }
+</style>
