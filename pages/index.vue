9a3b041789942221aec4a72938bdf92e1a64fa4b
RusKayskas <KayskasRus@yandex.ru>
2023-12-19 16:28:18 +0300

    add day js and animation

--- pages/index.vue
+++ pages/index.vue
@@ -1,8 +1,15 @@
 <script setup lang="ts">
-
+  useSeoMeta({
+    title: 'Home | Nuxt Crm'
+  });
 </script>
 <template>
   <div class="p-10">
     <h1 class="font-bold text-2xl mb-10">Главная Страница</h1>
+    <UiCard class="mb-3" draggable="true">
+      <UiCardHeader role="button">Name card</UiCardHeader>
+      <UiCardContent>Company</UiCardContent>
+      <UiCardFooter>Date</UiCardFooter>
+    </UiCard>
   </div>
 </template>
