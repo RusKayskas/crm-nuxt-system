4557ddff8d61bc4231c0383176d87103d46eed94
RusKayskas <KayskasRus@yandex.ru>
2023-12-19 03:21:05 +0300

    add logOut function

--- components/layout/Sidebar.vue
+++ components/layout/Sidebar.vue
@@ -1,10 +1,25 @@
+<script lang="ts" setup>
+    import { useAuthStore, useIsLoadingStore } from '@/store/auth.store';
+    const authStore = useAuthStore();
+    const router = useRouter();
+    const isLoadingStore = useIsLoadingStore();
+    //функция для выхода из системы
+    const logOut = async () => {
+        isLoadingStore.set(true);
+        //удаляем текущую сессию
+        await account.deleteSession('current');
+        authStore.clear();
+        await router.push('/login');
+        isLoadingStore.set(false);
+    }
+</script>
 <template>
     <aside class="px-5 py-8 bg-sidebar h-full relative w-full">
         <NuxtLink style="display: block;" to="/">
            <NuxtImg class="mx-auto" src="./logo.svg" alt="logo" width="120px"></NuxtImg>
         </NuxtLink>
         
-        <button class="absolute top-2 right-3 transition-colors hover:text-primary">
+        <button @click="logOut" class="absolute top-2 right-3 transition-colors hover:text-primary">
             <Icon name="line-md:logout" size="20px"/>
         </button>
         <LayoutMenu />
