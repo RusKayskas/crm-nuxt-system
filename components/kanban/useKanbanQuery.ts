e181925cacd4d44829d66b12a917d2dfa82468f8
RusKayskas <KayskasRus@yandex.ru>
2023-12-21 00:16:54 +0300

    add kanban data

---
+++ components/kanban/useKanbanQuery.ts
@@ -0,0 +1,42 @@
+import { useQuery } from "@tanstack/vue-query";
+import { COLLECTION_DEALS, DB_ID } from "~/app.constans";
+import { KANBAN_DATA } from "./kanban.data";
+import type { IDeal } from "~/types/deals.types";
+
+//запрос к бд на получение всех карточек и состояний
+export function useKanbanQuery() {
+  console.log('this', KANBAN_DATA);
+  return useQuery({
+    //назначаем ключ (должен быть уникальный)
+    queryKey: ['deals'],
+    //берем функцию query по которой получаем все наги доки из бд указываем id нашей базы данных и id нашей коллекции
+    queryFn: () => DB.listDocuments(DB_ID, COLLECTION_DEALS),
+     //получаем и записываем в колонку
+    select(data) {
+      //дублируем массив создаем на основе этого массива
+      const newBoard = [...KANBAN_DATA];
+      //конвертируем в нужную нам коллекцию
+      //принудительно указыввем что это является сделками
+      const deals = data.documents as unknown as IDeal[];
+      //проходимся циклом что бы распределить по статусам наши записи можно forEach но for Of быстрее и легче читаемый
+      for (const deal of deals) {
+        //получаем статусы с сервака ищем нужную колонку и в нее записываем данные
+        console.log('deals', deals);
+        const column = newBoard.find(col => col.id === deal.status);
+        console.log('kaban ', data);
+        if(column) {
+          column.items.push({
+            $createdAt: deal.$createdAt,
+            id: deal.$id,
+            name: deal.name,
+            price: deal.price,
+            companyName: deal.customer.name,
+            status: column.name
+          })
+        }
+      }
+      // //возвращаем обновленный newBoard
+      return newBoard
+    }
+  })
+}
--- pages/index.vue
+++ pages/index.vue
@@ -1,15 +1,34 @@
 <script setup lang="ts">
+import type { ICard, IColumn } from '@/components/kanban/kanban.types';
+import { useKanbanQuery } from '@/components/kanban/useKanbanQuery';
+
   useSeoMeta({
     title: 'Home | Nuxt Crm'
   });
+
+  // const dragCardRef = ref< ICard | null >(null);
+  // const sourceColumnRef = ref< IColumn | null >(null);
+  //получаем нужные поля refetch служит для переобновления данных
+  const { data, isLoading, refetch } =  useKanbanQuery();
 </script>
 <template>
   <div class="p-10">
     <h1 class="font-bold text-2xl mb-10">Главная Страница</h1>
-    <UiCard class="mb-3" draggable="true">
-      <UiCardHeader role="button">Name card</UiCardHeader>
-      <UiCardContent>Company</UiCardContent>
-      <UiCardFooter>Date</UiCardFooter>
-    </UiCard>
+    <div v-if="isLoading">Loading...</div>
+    <div v-else>
+      <div class="grid grid-col-5 gap-16">
+        <div v-for="column in data" :key="column.id">
+          <div class="rounded bg-slate-700 py-5 mb-2 text-center"> 
+            {{ column.name }}
+          </div>
+        </div>
+      </div>
+
+      <UiCard class="mb-3" draggable="true">
+        <UiCardHeader role="button">Name card</UiCardHeader>
+        <UiCardContent>Company</UiCardContent>
+        <UiCardFooter>Date</UiCardFooter>
+      </UiCard>
+    </div>
   </div>
 </template>
