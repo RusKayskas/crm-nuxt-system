c4738c01c37af5c67ee7d08859e767f06ab2fe73
RusKayskas <KayskasRus@yandex.ru>
2024-01-26 03:12:02 +0300

    add ui nuxt

---
+++ store/deals-slide.store.ts
@@ -0,0 +1,24 @@
+import type { ICard } from '~/components/kanban/kanban.types'
+import { defineStore } from 'pinia'
+
+const defaultValue: {card: ICard | null; isOpen: boolean}  = {
+  //по умолчагнию
+  card: null,
+  isOpen: false,
+}
+
+export const useDealSlideStore = defineStore('deal-slide', {
+  state: () => defaultValue,
+  actions: {
+    clear() {
+      this.$patch(defaultValue);
+    },
+    //показываем карточку
+    set(card: ICard) {
+      this.$patch({card, isOpen: true})
+    },
+    toggle() {
+      this.isOpen = !this.isOpen
+    }
+  },
+})
